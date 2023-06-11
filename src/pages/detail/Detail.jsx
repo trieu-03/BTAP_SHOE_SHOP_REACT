11580d74e38d384acc56397335473c3111405fcf
Trieu <tranbinhtrieuhgbt2004@gmail.com>
2023-06-11 12:40:12 +0700

    detail

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import './Detail.scss'
+//axios
+import axios from 'axios'
+//react
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+//redux
+import { setProductDetail } from '/src/redux/redux-slides/productListSlide'
+//component
+import DetailProduct from '/src/components/DetailProduct/DetailProduct'
+import ListItem from '../../components/listItem/ListItem'
+
+//---------------------------------------------------------------------------------
 
 function Detail() {
-  return (
-    <div>Detail</div>
-  )
+	const params = useParams()
+	const dispatch = useDispatch()
+
+	const { productDetail } = useSelector(state => state.productReducer)
+
+	const getProductById = async (id) => {
+		const result = await axios.get(`https://shop.cyberlearn.vn/api/Product/getbyid?id=${id}`)
+		const action = setProductDetail(result.data.content)
+		dispatch(action)
+	}
+
+	useEffect(() => {
+		getProductById(params.productID)
+	}, [params.productID])
+
+	return (
+		<div>
+			<DetailProduct />
+			<h3 className='relate-product'>- Relate Product -</h3>
+			<div className="row" style={{ marginLeft: '8.4rem', marginBottom: '10.6rem' }}>
+				<ListItem listProductOption={productDetail.relatedProducts || []} option />
+			</div>
+		</div>
+	)
 }
 
 export default Detail
--- src/redux/redux-slides/productListSlide.js
+++ src/redux/redux-slides/productListSlide.js
@@ -1,8 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const listCart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+const historyOrderList = localStorage.getItem('historyArr') ? JSON.parse(localStorage.getItem('historyArr')) : [];
+
 const initialState = {
     listProduct: [],
-    productDetail: {}
+    productDetail: {},
+    cartList: listCart,
+    orderHistoryList: historyOrderList,
 }
 
 const ProductSlice = createSlice({
@@ -11,14 +16,48 @@ const ProductSlice = createSlice({
     reducers: {
         setListProduct: (state, action) => {
             state.listProduct = action.payload;
-
         },
         setProductDetail: (state, action) => {
             state.productDetail = action.payload
-        }
+        },
+        setHistoryOrder: (state, action) => {
+            state.orderHistoryList.push(action.payload)
+
+            localStorage.setItem('historyArr',
+                JSON.stringify([...state.orderHistoryList, { list: action.payload.list, date: action.payload.date }]))
+        },
+        setToCart: (state, action) => {
+            const indexById = state.cartList.findIndex((product) => product.id === action.payload.id);
+            if (indexById === -1) {
+                state.cartList.push(action.payload)
+            } else {
+                state.cartList[indexById] = action.payload
+            }
+        },
+        changeCount: (state, action) => {
+            const indexById = state.cartList.findIndex((product) => product.id === action.payload.id);
+            state.cartList[indexById].count = action.payload.value;
+        },
+        removeProdCart: (state, action) => {
+            const indexById = state.cartList.findIndex((product) => product.id === action.payload);
+            state.cartList.splice(indexById, 1)
+
+            localStorage.setItem('cart', JSON.stringify(state.cartList))
+        },
     }
 });
 
-export const { setListProduct, setProductDetail } = ProductSlice.actions
+export const {
+    setListProduct,
+    setProductDetail,
+    setFavoriteProductList,
+    removeFavoriteProduct,
+    setToCart,
+    changeCount,
+    removeProdCart,
+    setHistoryOrder,
+
+} = ProductSlice.actions
 
 export default ProductSlice.reducer
+
